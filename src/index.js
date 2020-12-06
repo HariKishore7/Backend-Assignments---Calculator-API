fe75541f81b38ef52a1e29007c9c2be303ac706a
HariKishore7 <harikishoret99@gmail.com>
2020-12-06 11:36:18 +0530

    Changes made

--- src/index.js
+++ src/index.js
@@ -7,12 +7,133 @@ app.use(express.urlencoded());
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
+app.get('/',(req,res)=>{
+    res.write("Hello world!");
+    res.end();
+})
 
+app.post("/add",(req,res)=>{
+    const limit=1000000;
+    const num1 = Number(req.body.num1);
+    const num2 = Number(req.body.num2);
+    const message="the sum of given two numbers";
+    const sum = num1 + num2;
+    let status="success";
+    if(!num1 || !num2){
+        res.send({
+            status:"error",
+            message:"Invalid data types",
+        });
+    }
+    if(num1 < -limit || num2 < -limit || sum < -limit){
+        message="Underflow";
+        status="error";
+    }
+    if(num1 > limit || num2 > limit || sum > limit){
+        message="Overflow";
+        status="error";
+    }
+    res.send({
+        status:status,
+        message:message,
+        sum: sum,
+    });
+});
+
+app.post("/sub",(req,res)=>{
+    const limit=1000000;
+    const num1 = Number(req.body.num1);
+    const num2 = Number(req.body.num2);
+    const message="the difference of given two numbers";
+    const sum = num1 - num2;
+    let status="success";
+    if(!num1 || !num2){
+        res.send({
+            status:"error",
+            message:"Invalid data types",
+        });
+    }
+    if(num1 < -limit || num2 < -limit || sum < -limit){
+        message="Underflow";
+        status="error";
+    }
+    if(num1 > limit || num2 > limit || sum > limit){
+        message="Overflow";
+        status="error";
+    }
+    res.send({
+        status:status,
+        message:message,
+        sum: sum,
+    });
+});
+
+app.post("/multiply",(req,res)=>{
+    const limit=1000000;
+    const num1 = Number(req.body.num1);
+    const num2 = Number(req.body.num2);
+    const message="The product of given numbers";
+    const sum = num1 * num2;
+    let status="success";
+    if(!num1 || !num2){
+        res.send({
+            status:"error",
+            message:"Invalid data types",
+        });
+    }
+    if(num1 < -limit || num2 < -limit || sum < -limit){
+        message="Underflow";
+        status="error";
+    }
+    if(num1 > limit || num2 > limit || sum > limit){
+        message="Overflow";
+        status="error";
+    }
+    res.send({
+        status:status,
+        message:message,
+        sum: sum,
+    });
+});
+
+app.post("/divide",(req,res)=>{
+    const limit=1000000;
+    const num1 = Number(req.body.num1);
+    const num2 = Number(req.body.num2);
+    const message="The division of given numbers";
+    const sum = num1 / num2;
+    let status="success";
+    if(num2===0){
+        res.send({
+            status:"error",
+            message:"Cannot divide by zero"
+        });
+    }
+    if(!num1 || !num2){
+        res.send({
+            status:"error",
+            message:"Invalid data types",
+        });
+    }
+    
+    if(num1 < -limit || num2 < -limit || sum < -limit){
+        message="Underflow";
+        status="error";
+    }
+    if(num1 > limit || num2 > limit || sum > limit){
+        message="Overflow";
+        status="error";
+    }
+    res.send({
+        status:status,
+        message:message,
+        sum: sum,
+    });
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
